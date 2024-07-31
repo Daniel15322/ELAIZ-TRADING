a222fc073cfedac98929215bd2d5550423bf7b4b
Daniel15322 <dc8815322@gmail.com>
2024-07-31 22:07:24 +0200

    contact.php

---
+++ contact.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="awesome/css/all.min.css">
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js" async></script>
+    <title>Contacts</title>
+</head>
+<section class="contact" id="contact">
+    <div class="titre noir">
+        <h2 class="titre-texte"><span>C</span>ontact</h2>
+    </div>
+    <header>
+    <ul class="menu">
+        <a href="index.html">ACCUEIL</a>
+        <a href="Facultés.html">NOS AGENCES</a>
+        <a href="c.html">SERVICES</a>
+        <a href="Temoignage.html">EVALUATIONS</a>
+        <a href="Apparitorat.php">A PROPOS</a>
+    </ul>
+    </header>
+    <form class="form" id="form">
+    <div class="contactform">
+        <h3>Ecrivez-nous un message</h3>
+        <div class="inputboite ">
+            <input type="text" placeholder="Nom" id="nom">
+            <i class="fas fa-check-circle"></i>
+            <i class="fas fa-exclamation"></i>
+            <small>message d'erreur</small>
+        </div>
+        <div class="inputboite ">
+            <input type="text" placeholder="email" id="email">
+            <i class="fas fa-check-circle"></i>
+            <i class="fas fa-exclamation"></i>
+            <small>message d'erreur</small>
+        </div>
+        <div class="inputboite">
+            <textarea placeholder="Ecrivez-nous un message"></textarea>
+        </div>
+        <div class="inputboite">
+            <input type="submit" value="Envoyer">
+        </div>
+    </div>
+</form>
+</section>
+<div class="copyright">
+    <p> © copyright 2024 <a href="#">Elaiz</a> Tout droit reservé</p>
+</div>
+</body>
+</html>
