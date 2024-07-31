09f1dbd7d22a5210630fafc09537658e6e2153ca
Daniel15322 <dc8815322@gmail.com>
2024-07-31 21:10:53 +0200

    Apparitorat.php

---
+++ Apparitorat.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Apparitorat</title>
+</head>
+<section class="Apparitorat" id="Apparitorat">
+    <h1 class="title"><font color="orange">A PROPOS</font></h1>
+    <header>
+    <ul class="menu">
+            <a href="index.html">ACCUEIL</a>
+            <a href="Facultés.html">AGENCES</a>
+            <a href="c.html">SERVICES</a>
+            <a href="Temoignage.html">EVALUATIONS</a>
+            <a href="contact.php">SECRETARIAT</a>
+        </ul>
+    </header>
+    <p class="sub-title">
+        <br>  
+        <font color="white">Bienvenue chez ELAIZ TRADING SERVICES, basée en Afrique du Sud! Nous sommes une agence de frets, nous assurons le transport des colis.
+                Ces véhicules passent de l'Afrique du sud au Congo par divers pays notamment le Zimbabwe et la Zambie. 
+                Dans la ville de Lubumbashi, l'agence est située à la route karavia réf cercle hippique à proximité de l école Mwapusukeni. 
+ Nous faisons le transport des marchandises par des camions poids-lourds communément appelés TrucKs, à Lubumbashi se trouve un bureau de réception des clients et le dépôt de livraison des colis.                 En Afrique du sud se trouve le directeur général 
+Monsieur  Papy NTUMBUDILA ainsi un certains nombres d employés (secrétaire, chef du dépôt Monsieur  Philipe  NTUMBUDILA, d'un chargé des logistiques Arsène TSHIBANGU, chargeur du véhicules et les chauffeurs).</font>
+<br> <font color="white">Les colis sont taxés par kilo(2$ par kilo) ou par volume.</font> </p>
+    </p>
+    <br>
+    <p class="sub-title"><font color="white">Nous sommes ouverts du lundi au samedi de 8H00 à 15H30</font><br><b><font color="white">SOYEZ LES BIENVENUS</font></b></p>
+    <div class="list-Apparitorat">
+        <div class="box">
+            <h1><a href="connexion.php"> LOGIN</a></h1>
+        </div>
+        
+</section>
+<div class="copyright">
+    <p> © copyright 2024 <a href="#">Elaiz</a> Tout droit reservé</p>
+</div>
+</body>
+</html>
